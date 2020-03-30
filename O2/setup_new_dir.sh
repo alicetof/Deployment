eaa8a7f9fcc5127aeaf25cf4dff4dfddd3f1b02a
Nicolo' Jacazio <nicolo.jacazio@cern.ch>
2020-03-30 11:27:57 +0200

    Add new setup script for new installations

---
+++ O2/setup_new_dir.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Please enter path:"
+read NEWPATH
+echo "Setting new directory for O2 software in '$NEWPATH', is this OK (y/n)?"
+read ANS
+if [[ $ANS != "y" ]]; then
+  echo "Ok, aborting!"
+  exit 
+fi 
+
+if [[ ! -d $NEWPATH ]]; then
+  echo "ERROR: Check thad directory '$NEWPATH' exists and retry!"
+  exit
+fi
+
+TOCP="build.sh  clean.sh  enter.sh  init.sh  list.sh  o2suite_config.sh  pipupdate.sh  update.sh"
+set -x
+for i in $TOCP; do 
+  cp -i -v $i $NEWPATH/
+done
+set +x
+
