7dee872b05ee8e336b6b17db45c9bb096149aae5
Nicolo' Jacazio <nicolo.jacazio@cern.ch>
2020-03-31 09:28:01 +0200

    Split init in several lines, add remotes of alicetof

--- O2/init.sh
+++ O2/init.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
 source o2suite_config.sh
-aliBuild init O2@dev,QualityControl@master --defaults $O2DEFAULTS
+aliBuild init O2@dev, --defaults $O2DEFAULTS
+aliBuild init QualityControl@master --defaults $O2DEFAULTS
+aliBuild init TOFCommissioning@master --defaults $O2DEFAULTS
+
+function addremote(){
+  echo "Adding remote for $1: $2"
+  pushd $1
+  git remote add alicetof https://github.com/alicetof/$2.git
+  popd
+}
+
+addremote alidist alidist
+
+addremote QualityControl QualityControl
+
+addremote O2 AliceO2
 
