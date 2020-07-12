57f00778f882b15d63996d2dacbc7ddb4aedf8c6
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2020-07-12 10:01:36 +0200

    Add msgs

--- O2/build.sh
+++ O2/build.sh
@@ -3,5 +3,8 @@
 source o2suite_config.sh
 . pipupdate.sh
 
+echo "Building O2Suite"
 aliBuild build O2Suite --defaults $O2DEFAULTS
+
+echo "Building TOFCommissioning"
 aliBuild build TOFCommissioning --defaults $O2DEFAULTS
