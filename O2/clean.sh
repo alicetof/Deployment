2a1fd51753627bbfe0bec237d4003182da0ccd7d
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2020-07-14 17:01:46 +0200

    Update cleaning script

--- O2/clean.sh
+++ O2/clean.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 source o2suite_config.sh
-aliBuild clean
 
+if [[ -z $1 ]]; then
+  echo "Cleaning"
+  aliBuild clean
+else
+  echo "Aggressive cleaning"
+  aliBuild clean --aggressive-cleanup
+fi
