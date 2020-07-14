312a240262adc15e99f573bc958232576b1814dc
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2020-07-14 17:22:43 +0200

    Improve build logging

--- O2/build.sh
+++ O2/build.sh
@@ -5,7 +5,13 @@ source o2suite_config.sh
 
 function build_pckg(){
   echo "Building ${1}"
-  aliBuild build ${1} --defaults $O2DEFAULTS -d | tee ${i}_last_build.log
+  LOG_FILE="${1}_last_build.log"
+  CMD="aliBuild build ${1} --defaults $O2DEFAULTS -d"
+  date | tee $LOG_FILE
+  echo $CMD | tee -a $LOG_FILE
+  sleep 1
+  eval $CMD 2>&1 | tee -a $LOG_FILE
+  date | tee -a $LOG_FILE
 }
 
 build_pckg O2Suite
