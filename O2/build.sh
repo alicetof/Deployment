08ac0cfc399b4e738539a396cd1d77d6c0c7ca76
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2020-09-14 09:08:49 +0200

    Accepting all input for building
    - building only TOFCommissioning

--- O2/build.sh
+++ O2/build.sh
@@ -4,16 +4,18 @@ source o2suite_config.sh
 . pipupdate.sh
 
 function build_pckg(){
-  echo "Building ${1}"
-  LOG_FILE="${1}_last_build.log"
-  CMD="aliBuild build ${1} --defaults $O2DEFAULTS -d"
-  date | tee $LOG_FILE
-  echo $CMD | tee -a $LOG_FILE
-  sleep 1
-  eval $CMD 2>&1 | tee -a $LOG_FILE
-  date | tee -a $LOG_FILE
+    echo "Building ${1}"
+    LOG_FILE="${1}_last_build.log"
+    CMD="aliBuild build ${@} --defaults $O2DEFAULTS -d"
+    date | tee $LOG_FILE
+    echo $CMD | tee -a $LOG_FILE
+    sleep 1
+    eval $CMD 2>&1 | tee -a $LOG_FILE
+    date | tee -a $LOG_FILE
 }
 
-build_pckg O2Suite
+# build_pckg O2
+# build_pckg QualityControl
+# build_pckg O2Suite
 
 build_pckg TOFCommissioning
