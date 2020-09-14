ef54a60960a7dc0c989518563ed396c1a39ab4c5
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2020-09-14 09:07:19 +0200

    Update pgk updater
    - checking git version before updating with autostash

--- O2/update.sh
+++ O2/update.sh
@@ -2,18 +2,25 @@
 
 source o2suite_config.sh
 
-function doit(){
-	pkgdir=$1
-	echo "~~ Updating $pkgdir ~~"
-        sleep 2
-	cd $pkgdir
-	git pull --rebase --autostash
-	cd -
+function updatepkg(){
+    pkgdir=$1
+    echo "~~ Updating $pkgdir ~~"
+    sleep 2
+    cd $pkgdir
+    GIT_VERSION=$(git --version)
+    if [[ ${GIT_VERSION} == *" 2."* ]]; then
+        git pull --rebase --autostash
+    else
+        git stash
+        git pull --rebase
+        git stash apply
+    fi
+    cd -
 }
 
 toupdate="alidist $@"
 
 for i in $toupdate; do
-        doit $i
+    updatepkg $i
 done
 
